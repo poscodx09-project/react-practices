0f4e2fb8443980cc69a185006b108b81ab1d9c37
lej8924 <eunjae8924@naver.com>
2025-02-13 13:47:41 +0900

    🎨 Improve: useEffect finish

--- 07.Component - Lifecycle_useEffect/ex04/src/App.js
+++ 07.Component - Lifecycle_useEffect/ex04/src/App.js
@@ -3,11 +3,33 @@ import './assets/scss/App.scss'
 import Clock from './Clock';
 
 export default function App() {
-    return (
+
+    const getCurrentTime = ()=>{
+        const now = new Date();
+        return{
+            hours: now.getHours(),
+            mins : now.getMinutes(),
+            secs: now.getSeconds(),
+        }
+    }
+    const [currentTime,setCurrentTime] = useState(getCurrentTime());
+    const [ticks,setTicks] = useState(1);
+
+    useEffect(()=>{
+        const intervalId = setInterval(()=>{
+            setCurrentTime(getCurrentTime());
+            setTicks(ticks+1);
+        },1000);
+        return () => {
+            clearInterval(intervalId);
+        }
+    },[ticks]);
+
+    return ticks % 10 === 0 ? null:(
         <Clock
             title={`ex04: Clock Component II: ${ticks}`}
             hours={currentTime.hours}
-            minutes={currentTime.minutes}
-            seconds={currentTime.seconds} />
+            minutes={currentTime.mins}
+            seconds={currentTime.secs} />
     );
 }
